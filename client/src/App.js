ae4678b5a2a606b66febeef3ab566d34ea35a2e6
Uygar Süslü <uygarsuslu1@gmail.com>
2022-03-25 18:21:02 +0300

    e-commerce added

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import "./App.css";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import { home } from "nodemon/lib/utils";
+
+import Navbar from "./components/Navbar";
+import Signin from "./pages/Auth/Signin";
+import Signup from "./pages/Auth/Signup";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+
+        <div id="content">
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/signin" component={Signin} />
+            <Route path="/signup" component={Signup} />
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+export default App;
