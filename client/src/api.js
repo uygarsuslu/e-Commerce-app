dbd6f61c1d5ea5eeff53e2c08c69e134fda01f10
Uygar Süslü <uygarsuslu1@gmail.com>
2022-03-28 21:38:39 +0300

    api added

---
+++ client/src/api.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+export const fetchProductList = async ({ pageParam = 1 }) => {
+    const { data } = await axios.get(
+        `${process.env.REACT_APP_BASE_ENDPOINT}/product?page=${pageParam}`
+    );
+    return data;
+};
+
+export const fetchProduct = async (id) => {
+    const { data } = await axios.get(
+        `${process.env.REACT_APP_BASE_ENDPOINT}/product/${id}`
+    );
+    return data;
+}; 
