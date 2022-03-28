7f8fbee8b669ce953abdb19d9e194ca991176d5b
Uygar Süslü <uygarsuslu1@gmail.com>
2022-03-28 21:37:52 +0300

    products added

--- client/src/pages/Products/index.js
+++ client/src/pages/Products/index.js
@@ -1,21 +1,60 @@
-import Card from "../../components/Card"
-
-import { Grid } from "@chakra-ui/react";
+import React from "react";
+import Card from "../../components/Card";
+import { Grid, Box, Flex, Button } from "@chakra-ui/react";
+import { useInfiniteQuery } from "react-query";
+import { fetchProductList } from "../../api";
 
 function Products() {
-    return (
-        <div>
+  const {
+    data,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    status,
+  } = useInfiniteQuery("products", fetchProductList, {
+    getNextPageParam: (lastGroup, allGroups) => {
+      const morePagesExist = lastGroup?.length === 12;
 
+      if (!morePagesExist) {
+        return;
+      }
+      return allGroups.length + 1;
+    },
+  });
 
-            <Grid templateColumns='repeat(3, 1fr)' gap={4}>
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-            </Grid>
-        </div>
-    )
+  if (status === "isLoading") return "Loading...";
+  if (status === "error") return "An error has occurred: " + error.message;
+
+  return (
+    <div>
+      <Grid templateColumns="repeat(3, 1fr)" gap={4}>
+        {data &&
+          data.pages.map((group, i) => (
+            <React.Fragment key={i}>
+              {group.map((item) => (
+                <Box w="100%" key={item._id}>
+                  <Card item={item} />
+                </Box>
+              ))}
+            </React.Fragment>
+          ))}
+      </Grid>
+
+      <Flex mt="10" justifyContent="center">
+        <Button
+          onClick={() => fetchNextPage()}
+          isLoading={isFetchingNextPage}
+          disabled={!hasNextPage || isFetchingNextPage}
+        >
+          {isFetchingNextPage
+            ? "Loading more..."
+            : hasNextPage
+            ? "Load More"
+            : "Nothing more to load"}
+        </Button>
+      </Flex>
+    </div>
+  );
 }
-
-export default Products
+export default Products;
