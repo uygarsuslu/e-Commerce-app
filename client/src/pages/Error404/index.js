2fc4a74ace365f2e9c897d2045eb577001a3b912
Uygar Süslü <uygarsuslu1@gmail.com>
2022-04-01 21:30:50 +0300

    error 404 added

---
+++ client/src/pages/Error404/index.js
@@ -0,0 +1,14 @@
+import { Alert, AlertIcon, AlertTitle, AlertDescription, } from '@chakra-ui/react'
+
+function Error404() {
+    return (
+        <div>
+            <Alert status='error'>
+                <AlertIcon />
+                <AlertTitle mr={2}>Error404  !!!!</AlertTitle>
+                <AlertDescription>This page was not found.</AlertDescription>
+            </Alert>
+        </div>
+    )
+}
+export default Error404
